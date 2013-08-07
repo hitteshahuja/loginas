df0a5e0be4408f7d947c4a7c9d49dc544b2d27cb
= <=>
2013-08-08 00:35:53 +0100

    Added language strings for settings page

--- lang/en/local_loginas.php
+++ lang/en/local_loginas.php
@@ -1,3 +1,7 @@
 <?php
 $string['loginas'] = 'Login As';
 $string['pluginname'] = 'Login As';
+$string['enableloginasdesc'] = 'Enable Login As';
+$string['enableloginas'] = 'Enable Login As feature';
+$string['loginaslocations'] ='Select Locations';
+$string['loginaslocationsdesc'] ='Select Locations where you want the login as context menu to be shown';
